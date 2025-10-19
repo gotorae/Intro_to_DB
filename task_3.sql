f2c3810fe7bcd8cf508b5142f715bbaac77613d4
Effort Gotora <effortgotora123@gmail.com>
2025-10-19 17:26:08 +0200

    changes

---
+++ task_3.sql
@@ -0,0 +1,9 @@
+# Make sure you are using the database
+mycursor.execute("USE alx_book_store")
+
+# List all tables
+mycursor.execute("SHOW TABLES")
+
+print("The tables for alx_book_store are as below:")
+for table in mycursor.fetchall():
+    print(table)
